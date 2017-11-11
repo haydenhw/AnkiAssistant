ffc70e854cd10af94238e47a71009c172a811b60
hayden7913 <hayden321@gmail.com>
2017-11-10 17:44:03 -0800

    updated index.html

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function() {
   return(
     <div className="test">
-      Hola  Mundo
+      a new world order
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,5 @@ module.exports = {
         loaders: ["sass-loader"],
       },
     ],
-
    },
 }

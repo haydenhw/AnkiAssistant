1019ca462260301666efbe54199996d0958495f0
hayden7913 <hayden321@gmail.com>
2017-11-20 18:07:28 -0800

    renaming landing page classes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import LandingPageCard from './LandingPageCard';
+
 export default class App extends React.Component {
   constructor() {
     super();
@@ -18,7 +20,8 @@ export default class App extends React.Component {
 
   getHeroOpacity() {
     const { scrollY } = this.state;
-    return (1 - (scrollY / 750));
+    return 1;
+    return (1 - (scrollY / 200));
   }
 
   render() {
@@ -29,17 +32,63 @@ export default class App extends React.Component {
           <a className="header-logo" href=""></a>
           <button className={`${scrollY > 0 ? '' : 'header-button-hidden'} header-button onboard-button`}>GET STARTED</button>
         </div>
-        <section className="hero" style={{ opacity: this.getHeroOpacity() }}>
-          <div className="hero-circle">
+        <section className="hero" >
+          <div className="hero-circle" style={{ opacity: this.getHeroOpacity() }}>
             <div className="hero-circle-content">
               <h1 className="hero-title">Design cutting edge electroics in minutes.</h1>
               <button className="onboard-button">GET STARTED</button>
             </div>
           </div>
-      </section>
-      <section className="computer">
-      </section>
-    </div>
-  );
+        </section>
+        <section>
+          <div className="landing-page-section-heading">
+            <h2 className="landing-page-section-title">Bring your ideas to life</h2>
+            <p>Learn to create custom circuit board designs with PCBflow, save work in progress diagrams, and export them when you're ready to build the real thing.</p>
+          </div>
+          <section className="design-to-pcb-visual">
+            <div className="computer-wrapper">
+              <img className="computer-image inline-image" src="src/images/computer.png" alt="computer" />
+              <img className="design-tool-screenshot" src="src/images/design-tool-screenshot.png" alt="app screenshot" />
+            </div>
+            <img className="dots inline-image" src="src/images/dots.svg" alt="dots" />
+            <img className="less-dots inline-image" src="src/images/less-dots.svg" alt="dots" />
+            <img className="arrows inline-image" src="src/images/arrows.svg" alt="arrow" />
+            <img className="circuit-board-image inline-image" src="src/images/real-pcb.jpg" alt="circuit board" />
+          </section>
+        </section>
+        <section className="landing-page-card-section row">
+          <div className="landing-page-section-heading">
+            <h2>Design with ease</h2>
+            <p>PCBflow's user friendly drag and drop interface is a breeze to learn and use.</p>
+          </div>
+          <div className="landing-page-card-wrapper">
+            <div className="col4">
+              <LandingPageCard
+                className="landing-page-card"
+                icon="images/info-card-test-icon.png"
+                title="No Engineers Required"
+                content="PCBflow abstracts away the hard parts of printed circuit board design. You don’t need to worry about routing or connections."
+              />
+            </div>
+            <div className="col4">
+              <LandingPageCard
+                className="landing-page-card "
+                icon="images/info-card-test-icon.png"
+                title="Interactive Tutorial"
+                content="Our step by step tutorial will teach you how to reason about printed circuit board design while demostrating how to use the tool."
+              />
+            </div>
+            <div className="col4">
+              <LandingPageCard
+                className="landing-page-card "
+                icon="images/info-card-test-icon.png"
+                title="Instant Price Estimates"
+                content="Get a price estimate that updates automatically as you add and remove modules so you can keep your project under budget."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
---
+++ src/components/LandingPageCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function LandingPageCard(props) {
+  const { className, content, icon, title } = props;
+  return (
+    <div className={className}>
+      <div className="card-icon-frame">
+        <img src={icon} alt="info card icon" />
+      </div>
+      <h1 className>{title}</h1>
+      <p className>{content}</p>
+    </div>
+  );
+}
+
+LandingPageCard.propTypes = {
+  className: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};

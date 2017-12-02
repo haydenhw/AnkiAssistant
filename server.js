862b7df8b57afa62617a09090fd3fe3a703029d3
hayden7913 <hayden321@gmail.com>
2017-12-01 18:04:12 -0800

    server uses env.PORT

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static('dist'));
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.get('*', function(req, res) {
 //   res.sendFile(path.resolve(__dirname, 'public/index.html'));
 // });
 
-app.listen(3000, function(err) {
+app.listen(PORT, function(err) {
   if (err) {
     return console.error(err);
   }
 
-  console.log('Listening at http://localhost:3000/');
+  console.log('Listening at http://localhost:/' + PORT);
 });

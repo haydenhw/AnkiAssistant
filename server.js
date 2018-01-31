f3f7937429ef60e3a8896ca1045b2bea738e6589
hayden7913 <hayden321@gmail.com>
2018-01-31 10:40:46 -0800

    Cleans up redundant button styles

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const favicon = require('express-favicon');
 var app = express();
 var path = require('path');
 
-const PORT = process.env.PORT || 3000;
-// app.use(express.static(path.join(__dirname, 'dist')));
-app.use(favicon(__dirname + '/public/favicon.ico'));
+const PORT = process.env.PORT || 3001;
 app.use(express.static('dist'));
 
 app.listen(PORT, function(err) {
